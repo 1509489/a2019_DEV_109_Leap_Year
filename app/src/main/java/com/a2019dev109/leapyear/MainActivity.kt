ac4745e10e8c3c194579affccb038533ff3a875c
Justice <popystone@gmail.com>
2019-02-25 22:26:40 +0000

    Implemented logic for the text view to displays the correct message for the year being a leap year or not

--- app/src/main/java/com/a2019dev109/leapyear/MainActivity.kt
+++ app/src/main/java/com/a2019dev109/leapyear/MainActivity.kt
@@ -3,16 +3,31 @@ package com.a2019dev109.leapyear
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.EditorInfo
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), MainContract.View {
+    private lateinit var mainPresenter: MainPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainPresenter = MainPresenter(this)
     }
 
     override fun showIsLeapYearOrNot(isLeapYear: Boolean) {
+        val message: String
+        val year = etYear.text.toString()
 
+        if (isLeapYear){
+            message = "$year ${resources.getString(R.string.is_leap_year)}"
+            tvMessage.text = message
+        }
+        else{
+            message = "$year ${resources.getString(R.string.not_leap_year)}"
+            tvMessage.text = message
+        }
     }
 
     override fun showMessage(message: String) {
@@ -20,6 +35,12 @@ class MainActivity : AppCompatActivity(), MainContract.View {
     }
 
     fun onCheckYear(view: View){
-
+        when(view.id){
+            R.id.btnCheck ->{
+                val year = etYear.text.toString()
+                mainPresenter.isLeapYear(year)
+                etYear.onEditorAction(EditorInfo.IME_ACTION_DONE)
+            }
+        }
     }
 }
