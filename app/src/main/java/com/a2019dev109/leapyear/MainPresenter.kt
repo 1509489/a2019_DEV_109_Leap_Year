faab68e35d243b759f02c54801179fe92108dbaa
Justice <popystone@gmail.com>
2019-02-25 21:02:12 +0000

    Implemented logic to check if year is a negative value

--- app/src/main/java/com/a2019dev109/leapyear/MainPresenter.kt
+++ app/src/main/java/com/a2019dev109/leapyear/MainPresenter.kt
@@ -3,10 +3,16 @@ package com.a2019dev109.leapyear
 class MainPresenter(private val view: MainContract.View): MainContract.Presenter {
 
     override fun isLeapYear(year: String) {
-        when {
-            year.toInt() % 400 == 0 -> view.showIsLeapYearOrNot(true)
-            year.toInt() % 4 == 0 && year.toInt() % 100 != 0 -> view.showIsLeapYearOrNot(true)
-            else -> view.showIsLeapYearOrNot(false)
+        if (year.toInt() < 0){
+            view.showMessage("Year cannot be negative value")
+            view.showIsLeapYearOrNot(false)
+        }
+        else{
+            when {
+                year.toInt() % 400 == 0 -> view.showIsLeapYearOrNot(true)
+                year.toInt() % 4 == 0 && year.toInt() % 100 != 0 -> view.showIsLeapYearOrNot(true)
+                else -> view.showIsLeapYearOrNot(false)
+            }
         }
     }
 }
