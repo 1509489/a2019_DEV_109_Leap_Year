2a0c2a28b119c2f359f21bab88a4b9eb81d4a6c6
Justice <popystone@gmail.com>
2019-02-25 18:46:06 +0000

    Implemented test to check if years divisible by 400 are leap year

--- app/src/test/java/com/a2019dev109/leapyear/MainPresenterTest.kt
+++ app/src/test/java/com/a2019dev109/leapyear/MainPresenterTest.kt
@@ -4,16 +4,32 @@ import org.junit.Before
 import org.junit.Test
 
 import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.*
 import org.mockito.junit.MockitoJUnitRunner
 
 @RunWith(MockitoJUnitRunner::class)
 class MainPresenterTest {
+    private lateinit var mainPresenter: MainPresenter
+
+    @Mock lateinit var view: MainContract.View
 
     @Before
     fun setUp() {
+        mainPresenter = MainPresenter(view)
     }
 
     @Test
     fun isLeapYear() {
+    }
+
+    @Test
+    fun yearsDivisibleBy400IsLeapYear(){
+        val years = arrayOf("1600","2000", "2400")
+
+       for (year in years){
+           mainPresenter.isLeapYear(year)
+       }
+        verify(view, times(years.size)).showIsLeapYearOrNot(true)
     }
 }
