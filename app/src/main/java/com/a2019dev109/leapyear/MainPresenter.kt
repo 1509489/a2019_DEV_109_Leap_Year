78307ca8612597e652c14a8b02d4939e1a440e16
Justice <popystone@gmail.com>
2019-02-25 18:52:39 +0000

    Implemented logic to check if year is divisible by 400

--- app/src/main/java/com/a2019dev109/leapyear/MainPresenter.kt
+++ app/src/main/java/com/a2019dev109/leapyear/MainPresenter.kt
@@ -3,6 +3,8 @@ package com.a2019dev109.leapyear
 class MainPresenter(private val view: MainContract.View): MainContract.Presenter {
 
     override fun isLeapYear(year: String) {
-
+        if (year.toInt() % 400 == 0){
+            view.showIsLeapYearOrNot(true)
+        }
     }
 }
